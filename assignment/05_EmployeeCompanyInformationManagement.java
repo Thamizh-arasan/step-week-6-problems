b5513859a2b8c36475586ba2e7efcdfdf6f9cad6
Thamizh-arasan <thamizharasanvbye@gmail.com>
2026-09-20 20:32:58 +0530

    Add 05_EmployeeCompanyInformationManagement.java

---
+++ assignment/05_EmployeeCompanyInformationManagement.java
@@ -0,0 +1,28 @@
+public class EmployeeCompanyInformationManagement {
+    static class Employee {
+        String empName;
+        double salary;
+
+        static String companyName = "Bright Horizon Technologies";
+        static int employeeCount = 0;
+
+        Employee(String empName, double salary) {
+            this.empName = empName;
+            this.salary = salary;
+            employeeCount++;
+        }
+
+        static void printCompanyInfo() {
+            System.out.println(companyName);
+            System.out.println("Employees on record: " + employeeCount);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Employee("Divya", 65000);
+        new Employee("Arjun", 30000);
+        new Employee("Kiran", 50000);
+
+        Employee.printCompanyInfo();
+    }
+}
